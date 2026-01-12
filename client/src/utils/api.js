9b1d7bc862495b8166cef01337810e02cda05387
Pranish Ranjit <pranish492@gmail.com>
2026-01-12 22:00:23 +0545

    test

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL;
+// client/src/api.js
+const API_BASE_URL = 'https://portfolio-website-2jvr.onrender.com/api';
+
 
 
 const api = axios.create({
